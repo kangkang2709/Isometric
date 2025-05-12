eac4e0fd3bc4ba918253bc6011a45268b6bcc7da
kangkang2709 <khanhnguyen147348@gmail.com>
2025-05-12 22:41:02 +0700

    remove spring!!!

--- src/main/java/ctu/game/isometric/IsometricApplication.java
+++ src/main/java/ctu/game/isometric/IsometricApplication.java
@@ -2,29 +2,17 @@ package ctu.game.isometric;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 public class IsometricApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsometricApplication.class, args);
-	}
+		// Configure LibGDX window
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("CHRONO VEIL");
+		config.setWindowedMode(1280, 720);
+		config.setResizable(false);
 
-	@Bean
-	public CommandLineRunner gameRunner() {
-		return args -> {
-			// Configure LibGDX window
-			Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-			config.setTitle("CHRONO VEIL");
-			config.setWindowedMode(1280, 720);
-			config.setResizable(false);
-
-			// Launch the game
-			new Lwjgl3Application(new IsometricGame(), config);
-		};
+		// Launch the game
+		new Lwjgl3Application(new IsometricGame(), config);
 	}
 }
